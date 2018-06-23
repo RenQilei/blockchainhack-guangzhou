58cb9b98978f6eae92f7f4568f48461a946a3448
bigao <bigao@ebay.com>
2018-06-24 06:27:40 +0800

    warnings removed

--- contracts/case.sol
+++ contracts/case.sol
@@ -49,9 +49,9 @@ contract Case {
     mapping(address => Authorisation) public authorisations;
     
     // Organisation requests to modify
-    function requestToModifyFake(address patient) public constant returns (address requester, uint authWeight) {
+    function requestToModifyFake() public view returns (address requester, uint authWeight) {
         
-        authorisations[msg.sender].authWeight = 8;
+        require(authorisations[msg.sender].authWeight == 8);
         
         return (msg.sender, authorisations[msg.sender].authWeight);
     }
@@ -63,8 +63,8 @@ contract Case {
     }
     
     // User authorises to modify
-    function authToModifyFake(address authUser) public constant returns (address requester, uint authWeight) {
-        // require(authorisations[authUser].authWeight == 8);
+    function authToModifyFake(address authUser) public view returns (address requester, uint authWeight) {
+        require(authorisations[authUser].authWeight == 9);
         // authorisations[authUser].authWeight = 9;
         return (authUser, authorisations[authUser].authWeight);
     }
@@ -84,7 +84,7 @@ contract Case {
         authorisations[msg.sender].authWeight = 10;
     }
 
-    function readReport() public constant returns (
+    function readReport() public view returns (
         uint age, string gender, uint bodyTemperature, uint heartRate, bool isQunisy, string note, string prescription, address doctor, uint timestamp
     ) {
         require(authorisations[msg.sender].authWeight >= 9);
