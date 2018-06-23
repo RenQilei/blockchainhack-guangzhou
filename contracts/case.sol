e5c6b1b9949876a83dafdc61e1c33c69f9e2d5f1
Qilei Ren <renqilei@gmail.com>
2018-06-24 04:03:00 +0800

    .

--- contracts/case.sol
+++ contracts/case.sol
@@ -42,11 +42,19 @@ contract Case {
     
     // All reports for users
     Report[] reports;
+
+    uint index = 0;
     
     // All authorisations
     mapping(address => Authorisation) public authorisations;
     
     // Organisation requests to modify
+    function requestToModifyFake(address patient) public constant returns (address requester, uint authWeight) {
+        
+        authorisations[msg.sender].authWeight = 8;
+        
+        return (msg.sender, authorisations[msg.sender].authWeight);
+    }
     function requestToModify(address patient) public returns (address requester, uint authWeight) {
         
         authorisations[msg.sender].authWeight = 8;
@@ -55,6 +63,11 @@ contract Case {
     }
     
     // User authorises to modify
+    function authToModifyFake(address authUser) public constant returns (address requester, uint authWeight) {
+        // require(authorisations[authUser].authWeight == 8);
+        // authorisations[authUser].authWeight = 9;
+        return (authUser, authorisations[authUser].authWeight);
+    }
     function authToModify(address authUser) public returns (address requester, uint authWeight) {
         require(authorisations[authUser].authWeight == 8);
         authorisations[authUser].authWeight = 9;
@@ -66,18 +79,20 @@ contract Case {
     function modifyReport(uint age, string gender, uint bodyTemperature, uint heartRate, bool isQunisy, string note, string prescription) public {
         require(authorisations[msg.sender].authWeight == 9);
         
-        uint currentTime = now;
-        
-        reports[currentTime].age = age;
-        reports[currentTime].gender = gender;
-        reports[currentTime].bodyTemperature = bodyTemperature;
-        reports[currentTime].heartRate = heartRate;
-        reports[currentTime].isQunisy = isQunisy;
-        reports[currentTime].note = note;
-        reports[currentTime].prescription = prescription;
-        reports[currentTime].doctor = msg.sender;
-        reports[currentTime].timestamp = currentTime;
-        
+        reports.push(Report(age, gender, bodyTemperature, heartRate, isQunisy,note, prescription, msg.sender, now));
+        index = index + 1;
+        authorisations[msg.sender].authWeight = 10;
     }
-    
+
+    function readReport() public constant returns (
+        uint age, string gender, uint bodyTemperature, uint heartRate, bool isQunisy, string note, string prescription, address doctor, uint timestamp
+    ) {
+        require(authorisations[msg.sender].authWeight >= 9);
+        if (index == 0) {
+            return (0, "", 0, 0, false, "", "", address(0), 0);
+        } else {
+            uint latest = index - 1;
+            return (reports[latest].age, reports[latest].gender, reports[latest].bodyTemperature, reports[latest].heartRate, reports[latest].isQunisy, reports[latest].note,reports[latest].prescription,reports[latest].doctor,reports[latest].timestamp);
+        }
+    }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ App = {
   contracts: {},
 
   init: function() {
+    // Load pets.
     return App.initWeb3();
   },
 
@@ -10,24 +11,21 @@ App = {
     /*
      * Replace me...
      */
-
-    if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider;
-    } else {
+     // Is there an injected web3 instance?
+      if (typeof web3 !== 'undefined') {
+          App.web3Provider = web3.currentProvider;
+      } else {
       // If no injected web3 instance is detected, fall back to Ganache
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-    }
-
-    web3 = new Web3(App.web3Provider);
-
-    return App.initContract();
+      }
+      web3 = new Web3(App.web3Provider);
+      return App.initContract();
   },
 
   initContract: function() {
     /*
      * Replace me...
      */
-
     $.getJSON('Case.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with truffle-contract
       var CaseArtifact = data;
@@ -36,53 +34,115 @@ App = {
       // Set the provider for our contract
       App.contracts.Case.setProvider(App.web3Provider);
     
-      // // Use our contract to retrieve and mark the adopted pets
+      // Use our contract to retrieve and mark the adopted pets
       // return App.markAdopted();
     });
-
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '#btn-init-case', App.handleInitCase);
     $(document).on('click', '#btn-request-modify', App.handleRequest);
+    $(document).on('click', '#btn-accept-auth', App.handleAuthRequest);
+    $(document).on('click', '#btn-request-diagnose', App.handleDiagnose);
+    // $('#case-display').ready(App.handleReadReport);
+    $(document).on('click', '#btn-request-read', App.handleReadReport);
+    $(document).on('click', '#btn-input-submit', App.handleModifyReport);
   },
 
-  handleInitCase: function(event) {
-
-    let caseInstance;
-    
+  handleModifyReport: function() {
+    var caseInstance;
     web3.eth.getAccounts(function(error, accounts) {
-      let user = accounts[0];
+      if (error) {
+        console.log("1");
+          console.log(error);
+      }
+      var account = accounts[0];
+      console.log(account);
 
       App.contracts.Case.deployed().then(function(instance) {
-        caseInstance = instance;
+          caseInstance = instance;
+          // Execute adopt as a transaction by sending account
+          return caseInstance.modifyReport(
+            $('#input-age').val(),
+            $('#input-gender').val(),
+            $('#input-temperature').val(),
+            $('#input-heart-rate').val(),
+            $('#input-qunisy').val(),
+            $('#input-note').val(),
+            $('#input-medicine').val(),
+            {from: account}
+          );
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(function(err) {
+          console.log(err.message);
+      });
+    });
+  },
+  handleReadReport: function() {
+    var caseInstance;
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log("1");
+          console.log(error);
+      }
+      var account = accounts[0];
+      console.log(account);
 
+      App.contracts.Case.deployed().then(function(instance) {
+          caseInstance = instance;
+          // Execute adopt as a transaction by sending account
+          return caseInstance.readReport();
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(function(err) {
+          console.log(err.message);
+      });
+    });
+  },
+  handleAuthRequest: function(event) {
+    event.preventDefault();
+    var caseInstance;
+    var doctorAddress = $("#input-doctor-address").val();
+    console.log(doctorAddress);
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log("1");
+          console.log(error);
+      }
+      var account = accounts[0];
+      console.log(account);
+
+      App.contracts.Case.deployed().then(function(instance) {
+          caseInstance = instance;
+          // Execute adopt as a transaction by sending account
+          return caseInstance.authToModify(doctorAddress);
+      })
+      .then((res) => {
+        return caseInstance.authToModifyFake(doctorAddress);
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(function(err) {
+          console.log(err.message);
       });
     });
   },
 
   handleRequest: function(event) {
-    event.preventDefault();
+    // event.preventDefault();
 
-    let caseInstance;
-    let patientAddress = $("#input-patient-address").val();
-
+    var caseInstance;
+    var patientAddress = $("#input-patient-address").val();
     console.log(patientAddress);
-
-    // App.contracts.Case.deployed().then(function(instance) {
-    //   caseInstance = instance;
-
-    //   // Execute adopt as a transaction by sending account
-    //   return caseInstance.requestToModify(account);
-    // }).then(function(result) {
-    //     console.log("success: " + result);
-    // }).catch(function(err) {
-    //     console.log("falied: " + err.message);
-    // });
-
     web3.eth.getAccounts(function(error, accounts) {
       if (error) {
+        console.log("1");
           console.log(error);
       }
       var account = accounts[0];
@@ -92,17 +152,19 @@ App = {
           caseInstance = instance;
 
           // Execute adopt as a transaction by sending account
-          return caseInstance.requestToModify(account);
-      }).then(function(result) {
-          console.log(result);
-      }).catch(function(err) {
+          return caseInstance.requestToModify(patientAddress);
+      })
+      .then((res) => {
+        return caseInstance.requestToModifyFake(patientAddress);
+      })
+      .then((res) => {
+        console.log(res);
+        $('#display-address').html(res[0] + '<br />' + res[1]['c'][0]);
+      })
+      .catch(function(err) {
           console.log(err.message);
       });
     });
-
-    /*
-     * Replace me...
-     */
   }
 
 };
